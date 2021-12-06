e1982b5b3eb0242aec61377947f16df7f63ff520
daichen@py <chen.dai@pengyunnetwork.com>
2021-12-06 11:59:23 +0800

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <highcharts :options="chartOptions" :callback="myCallback">
+          
+    </highcharts>
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      chartOptions: {
+        chart: {
+          height: 600,
+          inverted: true
+        },
+        title: {
+          text: 'Highsoft 公司组织结构'
+        },
+  
+        series: [{
+          type: 'organization',
+          name: 'Highsoft',
+          keys: ['from', 'to'],
+          data: [
+            ['CEO', 'CTO'],
+            ['CEO', 'COO'],
+            ['CEO', 'CSO'],
+            ['CEO', 'CMO'],
+          //  ['CTO', 'YF-111'],
+            ['COO', 'RS-3']
+          ],
+          levels: [
+            {
+              level: 0,
+              color: '#980104'
+            }, {
+              level: 1,
+              color: '#359154'
+            }, {
+              level: 2,
+              color: 'pink'
+            },
+            {
+              level: 4,
+              color: 'green'
+            }
+          ],
+          nodes: [{
+            id: 'Shareholders'
+          }, {
+            id: 'Board'
+          }, {
+            id: 'CEO',
+            title: 'CEO',
+            name: '陈靓',
+            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132317/Grethe.jpg'
+          },{
+            id: 'CTO',
+            title: 'CTO  （产品研发）',
+            name: '王中原',
+            column: 1,
+            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12140620/Christer.jpg',
+            layout: 'hanging',
+          }, {
+            id: 'COO',
+            title: 'COO  （职能）',
+            name: '张雪莹',
+            column: 1,
+            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12131849/Torstein1.jpg',
+             layout: 'hanging'
+          }, {
+            id: 'CSO',
+            title: '常务副总经理 （销售）',
+            name: '刘军',
+            column: 1,
+            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132313/Anita.jpg',
+            layout: 'hanging'
+          }, {
+            id: 'CMO',
+            title: '产品（技术支撑）',
+            name: 'Vidar Brekke',
+            column: 1,
+            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/13105551/Vidar.jpg',
+            layout: 'hanging'
+          }, 
+          {
+            id: 'RS-3',
+            title: ' 前台',
+            name: '江天禛',
+            column: 2,
+            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/13105551/Vidar.jpg'
+          }],
+          colorByPoint: false,
+          color: '#007ad0',
+          dataLabels: {
+            color: 'white'
+          },
+          borderColor: 'white',
+          nodeWidth: 65
+        }],
+        tooltip: {
+          outside: true
+        },
+        exporting: {
+          allowHTML: true,
+          sourceWidth: 800,
+          sourceHeight: 600
+        }
+      }
+    }
+  },
+  mounted () {},
+  methods: {
+    myCallback () {
+      console.log('this is callback function')
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.highcharts-container {
+  width: 600px;
+  height: 400px;
+  border: 1px solid #ddd;
+  margin: auto;
 }
 </style>
